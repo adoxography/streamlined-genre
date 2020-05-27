14c80f14faf1984067bd079092d19b1514105759
Graham Still <gstill@uw.edu>
2020-05-26 22:34:12 -0500

    Restructure setup.py

---
+++ genre/__version__.py
@@ -0,0 +1,5 @@
+""" genre.__version__ """
+VERSION = (0, 1, 0)
+
+
+__version__ = '.'.join(map(str, VERSION))
--- setup.py
+++ setup.py
@@ -1,42 +1,62 @@
 #!/usr/bin/env python3
+import io
 import os
-from setuptools import setup
+from setuptools import find_packages, setup
 
 
-def read_file(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+NAME = 'streamlined-genre'
+DESCRIPTION = ''
+URL = 'https://github.com/adoxography/streamlined-genre'
+EMAIL = 'gstill@uw.edu'
+AUTHOR = 'Graham Still'
+REQUIRES_PYTHON = '>=3.6.0'
 
+REQUIRED = [
+    'liac-arff==2.4.0',
+    'librosa==0.7.2',
+    'nlpaug==0.0.14',
+    'numpy==1.18.4',
+    'scipy==1.4.1',
+    'sklearn==0.0',
+    'wget==3.2'
+]
+
+EXTRAS = {
+    'dev': [
+        'flake8',
+        'ipython',
+        'pylint'
+    ]
+}
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with io.open(os.path.join(here, 'README.md'), encoding='utf-8') as readme_file:
+    long_description = '\n' + readme_file.read()
+
+about = {}
+with open(os.path.join(here, 'genre', '__version__.py')) as version_file:
+    exec(version_file.read(), about)  # pylint: disable=exec-used
+version = about['__version__']
 
 setup(
-    name='streamlined-genre',
-    version='0.0.1',
-    author='Graham Still',
-    author_email='gstill@uw.edu',
-    description='',
+    name=NAME,
+    version=version,
+    author=AUTHOR,
+    author_email=EMAIL,
+    description=DESCRIPTION,
+    python_requires=REQUIRES_PYTHON,
+    packages=find_packages(),
     license='GPL-3.0',
     keywords='nlp genre classification speech low-resource languages',
-    url='https://github.com/adoxography/streamlined-genre',
-    install_requires=[
-        'liac-arff==2.4.0',
-        'librosa==0.7.2',
-        'nlpaug==0.0.14',
-        'numpy==1.18.4',
-        'scipy==1.4.1',
-        'sklearn==0.0',
-        'wget==3.2'
-    ],
-    extras_require={
-        'dev': [
-            'flake8',
-            'ipython',
-            'pylint'
-        ]
-    },
-    long_description=read_file('README.md'),
+    url=URL,
+    install_requires=REQUIRED,
+    extras_require=EXTRAS,
+    long_description=long_description,
     classifiers=[
         'Environment :: Console',
         'Intended Audience :: Science/Research',
-        'License :: OSI Approved :: MIT License',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
         'Programming Language :: Python',
         'Topic :: Scientific/Engineering :: Artificial Intelligence'
     ]
